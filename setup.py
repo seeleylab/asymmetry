8da7832a80dc5e082a3fdc8824d228f634f5bee2
Jersey Deng <jersdeng@gmail.com>
2017-01-25 11:24:15 -0800

    revert to 0d2798b

--- setup.py
+++ setup.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python
 
 from setuptools import setup
-import os
 
 setup(
 	name='asymmetry',
-	version='0.1',
-	packages=['asymmetry'],
-	scripts=[os.path.join('scripts', 'fslxmean')],
-	install_requires=[],
 	author='Jersey Deng',
-	author_email='jersey.deng@ucsf.edu',
-	description='Collection of scripts used in asymmetry project'
+	author_email='Jersey.Deng@ucsf.edu',
+	version='0.1',
+	packages=['asymmetry']
 )
