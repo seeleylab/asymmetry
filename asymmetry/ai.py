63900806bbb7c643cac715786e7974e71d7b58e5
Jersey Deng <jersdeng@gmail.com>
2017-03-22 10:05:18 -0700

    latest changes

--- asymmetry/ai.py
+++ asymmetry/ai.py
@@ -2,14 +2,8 @@ import numpy as np
 import pandas as pd
 
 
-all_matrix = np.loadtxt('/data/mridata/jdeng/tools/asymmetry/data/brainnetome_regional_wscores.txt')
-left_regions = np.arange(1,247,2) - 1
-right_regions = np.arange(2,247,2) - 1
-left_matrix = all_matrix[:,left_regions]
-right_matrix = all_matrix[:,right_regions]
-
-# values
-ai = np.abs(left_matrix - right_matrix)
+### Create writer object ###
+writer = pd.ExcelWriter('/data/mridata/jdeng/tools/asymmetry/data/brainnetome_regional_ai.xlsx')
 
 # column names
 with open('/data/mridata/ejkim/BrainnetomeALL_v1_Beta_20160106 2/Testatlas.txt', 'r') as fin:
@@ -20,7 +14,23 @@ regions_123 = [regions_246[i].replace('_R_', '').replace('_L_', '') for i in ran
 with open('/data/mridata/jdeng/tools/asymmetry/asymmetry/subjs_439.txt', 'r') as fin:
     subjs = [line.strip() for line in fin.readlines()]
 
-# combine and save
+### AI spreadsheet ###
+all_matrix = np.loadtxt('/data/mridata/jdeng/tools/asymmetry/data/brainnetome_regional_wscores.txt')
+left_regions = np.arange(1,247,2) - 1
+right_regions = np.arange(2,247,2) - 1
+left_matrix = all_matrix[:,left_regions]
+right_matrix = all_matrix[:,right_regions]
+
+ai = np.abs( (left_matrix - right_matrix) / (left_matrix + right_matrix) )
+
 df = pd.DataFrame(ai, index=subjs, columns=regions_123)
-df.to_excel('/data/mridata/jdeng/tools/asymmetry/data/brainnetome_regional_ai.xlsx',
-            sheet_name='Sheet1')
+df.to_excel(writer, sheet_name='AI')
+
+### wscore spreadsheet ###
+wscores = np.loadtxt('/data/mridata/jdeng/tools/asymmetry/data/brainnetome_regional_wscores.txt')
+
+df = pd.DataFrame(wscores, index=subjs, columns=regions_246[:-1])  # get rid of last label (Spheric_ROI)
+df.to_excel(writer, sheet_name='wscores')
+
+### Save spreadsheet ###
+writer.save()
